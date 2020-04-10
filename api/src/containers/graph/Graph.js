1f4f2c4a47fe5e9843896635bf1049c2c649c9a4
Francisco Medina <francisco@Franciscos-iMac-2.local>
2020-04-10 17:21:46 -0400

    fix relative values

--- api/src/containers/graph/Graph.js
+++ api/src/containers/graph/Graph.js
@@ -12,13 +12,19 @@ import { appendChildren, scaleFactorConverter, getTemplateAreasColumns, getMagni
 export class Graph {
     constructor(historicalData) {
         this.historicalData = historicalData;
+        this.historicalData.forEach((hD, index) => {
+            for (let i = 0; i < index; i++) {
+                // hD.positive -= this.historicalData[i].positive;
+            }
+        });
         this.numberOfColumns = this.historicalData.length + 1;
         this.highestNumber = this.findHighestValue();
         this.magnitude = getMagnitude(this.highestNumber);
-        this.numberOfRows = Math.ceil(this.highestNumber / this.magnitude) * this.magnitude;
-        console.log(historicalData, '\n\nnumberOfColumns: ' + this.numberOfColumns, '\nhighestNumber: ' + this.highestNumber, '\nmagnitude: ' + this.magnitude, '\nnumberOfRows: ' + this.numberOfRows);
+        this.numberOfRows = Math.floor(this.highestNumber / this.magnitude) * this.magnitude;
         this.view = elementFromHTMLString('<span class=graph__view></span>');
+        this.view.style.height = (Math.ceil(this.highestNumber / this.magnitude) * this.magnitude) / 10 + 'rem';
         this.renderLine();
+        console.log('highestNumber', this.highestNumber, 'this.magnitude', this.magnitude);
     }
     renderLine() {
         this.yAxis = new YAxis(this.numberOfRows, this.magnitude);
@@ -27,8 +33,9 @@ export class Graph {
         this.view.style.gridTemplateColumns = 'repeat(' + this.numberOfColumns + ', 1fr)';
         appendChildren(this.view,
             this.yAxis.view,
-            ...this.historicalData.map(dataPoint => {
-                const dataElement = new DataPointElement(dataPoint.positive, this.numberOfRows, this.highestNumber).view;
+            ...this.historicalData.map((dataPoint, index) => {
+                let numberValue = dataPoint.positive;
+                const dataElement = new DataPointElement(numberValue, this.numberOfRows, this.highestNumber).view;
                 return dataElement;
             }),
             this.xAxis.view
@@ -47,10 +54,9 @@ export class Graph {
 
 class DataPointElement {
     constructor(numberValue, numberOfRows, highestNumber) {
-        const character = '*';
         const convertionFactor = highestNumber / numberOfRows;
         this.view = elementFromHTMLString('<img src="./api/src/img/Covid-dark.png" class=graph__dataPoint></img>');
-        this.view.style.marginBottom = scaleFactorConverter(numberValue, convertionFactor) + 'rem';
+        this.view.style.marginBottom = `${numberValue / 10}rem`;
         this.view.title = numberValue;
     }
 }
@@ -58,17 +64,21 @@ class YAxis {
     constructor(numberOfItems, magnitude) {
         const numberOfRows = (numberOfItems / magnitude);
         const gridArea = 'yAxis__r';
-        let gridAreas = '';
+        let gridAreas = 'test';
         this.view = appendChildren(elementFromHTMLString(`<span class=graph__yAxis></span>`),
             ...Array.apply(null, Array(numberOfRows)).map((n, index) => {
-                const rowElement = elementFromHTMLString(`<h3 class=yAxis__row>${numberOfRows * ((numberOfRows - (index)) / numberOfRows) * 100}</h3>`);
-                rowElement.style.gridArea = gridArea + index;
-                gridAreas += `"${gridArea + index} "`;
+                const value = numberOfRows * ((numberOfRows - (index)) / numberOfRows) * magnitude;
+                const rowElement = elementFromHTMLString(`<h3 class=yAxis__row>${value}</h3>`);
+                // rowElement.style.gridArea = gridArea + index;
+                // rowElement.style.gridArea = gridArea;
+                // gridAreas += `"${gridArea + index} "`;
+                rowElement.style.bottom = value / 10 + 'rem';
                 return rowElement;
             })
         );
+        // this.view.style.gridTemplateAreas = gridAreas;
         this.view.style.gridTemplateAreas = gridAreas;
-        this.view.style.gridTemplateRows = 'repeat(' + numberOfRows + ', 1fr)';
+        // this.view.style.gridTemplateRows = 'repeat(' + numberOfRows + ', 1fr)';
     }
 }
 class XAxis {
